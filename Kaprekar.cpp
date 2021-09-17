86cf11d80c6d8086c2dbc2a0c22620549eec04f3
ASH (Santiago Valderrama) <69223535+ASHVALDE@users.noreply.github.com>
2021-09-17 17:41:19 -0500

    Explicacion ejercicio Kaprekar

--- Kaprekar.cpp
+++ Kaprekar.cpp
@@ -1,6 +1,22 @@
+//[[
+// El matemático indio Dattaraya Ramchandra Kaprekar descubrió en 1949 una curiosa característica del número 6174. Hoy, se conoce a dicho número como constante de Kaprekar en honor a él.
+//El número es notable por la siguiente propiedad:
+//Elige un número de cuatro dígitos que tenga al menos dos diferentes (es válido colocar el dígito 0 al principio, por lo que el número 0009 es válido).
+//Coloca sus dígitos en orden ascendente y en orden descendente para formar dos nuevos números. Puedes añadir los dígitos 0 que necesites al principio.
+//Resta el menor al mayor.
+//Vuelve al paso 2.
+//A este proceso se le conoce como la rutina de Kaprekar, y siempre llegará al número 6174 en, como mucho, 7 iteraciones. Una vez en él, el proceso no avanzará, dado que 7641 − 1467 = 6174.
+//
+//Por ejemplo, el número 3524 alcanzará la constante de Kaprekar en 3 iteraciones:
+//
+//5432 − 2345 = 3087
+//8730 − 0378 = 8352
+//8532 − 2358 = 6174
+// ]]
+
+
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
@@ -43,9 +59,17 @@ int SortedNumber(int a,int b,int c,int d){
 }
 
 int main(){
+
     int resultado = 3524;
+    int iteraciones =0;
+    cout<<"Ingrese un numero de 4 cifras: ";
+    cin>>resultado;
+    cout<<endl;
     do{
         resultado = SortedNumber(integerToArray(resultado)[0],integerToArray(resultado)[1],integerToArray(resultado)[2],integerToArray(resultado)[3]);
         cout<<resultado<<endl;
+        iteraciones++;
     }while(resultado!=6174);
+
+    cout<<endl<<"Este numero tiene "<<iteraciones<<" iteraciones";
 }
